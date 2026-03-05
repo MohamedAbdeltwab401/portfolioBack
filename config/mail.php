ca01564c9c3a2001286ddba5ca649e1ce54baf48
MohamedAbdeltwab401 <mohasd139@gmail.com>
2026-03-05 13:16:32 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -15,7 +15,6 @@ return [
     */
 
     'default' => env('MAIL_MAILER', 'log'),
-    'portfolio_owner' => env('PORTFOLIO_OWNER_EMAIL', 'midosaed62@gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -40,13 +39,12 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 60,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
