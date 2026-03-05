1b3ca154f5b4c120b88825af5b2176ca8b725f40
MohamedAbdeltwab401 <mohasd139@gmail.com>
2026-03-05 12:53:01 +0200

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,16 +24,16 @@ public function send(Request $request)
     ]);
 }
 
-public function recieve(Request $request) {
-        $data = $request->validate([
-        'name' => 'required',
-        'email' => 'required|email',
-        'message' => 'required'
-    ]);
-    Mail::to(config('mail.portfolio_owner'))->send(new ContactMail($data));
-    return response()->json([
-        'status' => true,
-        'message' => 'Email sent successfully'
-    ]);
-}
+// public function recieve(Request $request) {
+//         $data = $request->validate([
+//         'name' => 'required',
+//         'email' => 'required|email',
+//         'message' => 'required'
+//     ]);
+//     Mail::to($request->email)->send(new ContactMail($data));
+//     return response()->json([
+//         'status' => true,
+//         'message' => 'Email sent successfully'
+//     ]);
+// }
 }
